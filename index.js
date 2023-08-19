31adeb6a3bbc1e6589f693e27bd7a841ea0b33f0
Luisa Eysele <eysele.luisa@gmail.com>
2023-08-18 18:44:09 -0600

    Práctica de búsqueda binaria

--- index.js
+++ index.js
@@ -12,13 +12,18 @@ let inicio = 0;
 let fin = array.length - 1;
 
 
-// se crea función de búsqueda binaria
+// se crea función flecha para definir el valor que se encuentra en la mitad del arreglo
 let iniciar = () => {
     mitad = Math.floor((inicio + fin) / 2);
     start.style.display = "none";
+    esMenor.style.display = "inline";
+    esMayor.style.display = "inline";
+    esCorrecto.style.display = "inline";
+    refresh.style.display = "inline";
     quest.innerHTML = `¿Tu número es ${mitad}?`;
 }
 
+// se crea función confirmar para botón
 let confirmar = () =>{
     quest.innerHTML = `Tu numero es: ${mitad}, ¡excelente decisión!`;
     esMenor.style.display = "none"
@@ -26,16 +31,19 @@ let confirmar = () =>{
     esCorrecto.style.display = "none"
 }
 
+// caso en el que el valor que piense el usuario sea menor que el indicado por el algoritmo
 let menorQue = () =>{
     fin = mitad - 1;
     iniciar();
 }
 
+// caso en el que el valor que piense el usuario sea menor que el indicado por el algoritmo
 let mayorQue = () =>{
     inicio = mitad + 1;
     iniciar();
 }
 
+// función para recargar el juego
 let refresh = document.getElementById('reload');
 refresh.addEventListener('click', () => {
             location.reload();
